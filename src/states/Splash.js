0ad64d18cfe084e80c53abb1a567ebe3dbcd6a1c
Robby <ta7382@gmail.com>
2018-03-28 01:24:05 +0800

    Update load resource

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -6,18 +6,33 @@ import {
 export default class extends Phaser.State {
   init() {}
 
+  loadSpritesheet() {
+    this.load.spritesheet('player', 'assets/images/player.png', 32, 32)
+    this.load.spritesheet('conveyorRight', 'assets/images/conveyor_right.png', 96, 16);
+    this.load.spritesheet('conveyorLeft', 'assets/images/conveyor_left.png', 96, 16);
+    this.load.spritesheet('trampoline', 'assets/images/trampoline.png', 96, 22);
+    this.load.spritesheet('fake', 'assets/images/fake.png', 96, 36);
+  }
+
+  loadImage() {
+    this.load.image('wall', 'assets/images/wall.png')
+    this.load.image('ceiling', 'assets/images/ceiling.png')
+    this.load.image('normal', 'assets/images/normal.png');
+    this.load.image('nails', 'assets/images/nails.png');
+  }
+
   preload() {
     this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
     this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
     centerGameObjects([this.loaderBg, this.loaderBar])
 
     this.load.setPreloadSprite(this.loaderBar)
-    //
-    // load your assets
-    //
-    this.load.image('mushroom', 'assets/images/mushroom2.png')
-    this.load.image('sky', 'assets/images/sky.png')
-    this.load.image('ground', 'assets/images/platform.png')
+
+    this.load.crossOrigin = 'anonymous'
+
+    this.loadImage()
+    this.loadSpritesheet()
+
   }
 
   create() {
